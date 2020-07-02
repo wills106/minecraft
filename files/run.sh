5d5315c721ae309873609c1dffcf2b65ef524384
fithwum <34775371+fithwum@users.noreply.github.com>
2020-07-01 22:17:49 -0700

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -2,5 +2,4 @@
 # Copyright (c) 2018 fithwum
 # All rights reserved
 
-MC_VERSION=1.16.1
-java -Xmx2048M -Xms2048M -jar minecraft_server_${MC_VERSION}.jar nogui
+java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
