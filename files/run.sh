9db52b55caf8be588ef112bd1ff889f9b545b535
fithwum <34775371+fithwum@users.noreply.github.com>
2020-07-02 10:07:04 -0700

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -3,4 +3,4 @@
 # All rights reserved
 
 cd /MCserver
-java -Xmx4G -Xms1024M -jar ./MCserver.jar nogui
+java -Xmx4G -Xms1024M -Xmn1G -XX:PermSize=512m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar ./MCserver.jar nogui
