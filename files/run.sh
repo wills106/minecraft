950316c987f171e357ef0097322ce2fa5c76a7bd
fithwum <34775371+fithwum@users.noreply.github.com>
2020-10-27 20:16:24 -0700

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -2,5 +2,13 @@
 # Copyright (c) 2020 fithwum
 # All rights reserved
 
+:begin
+
 cd /MCserver
 java -Xmx4G -Xms1024M -Xmn1G -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar ./MCserver.jar nogui
+
+timeout 10
+
+echo resuming server...
+
+goto begin
