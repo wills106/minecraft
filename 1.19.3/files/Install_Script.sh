4e4d2df5a63d9db77140658b00283529d5f116a9
fithwum <fithwum@hotmail.com>
2023-03-20 08:43:39 -0700

    updates

--- 1.19.3/files/Install_Script.sh
+++ 1.19.3/files/Install_Script.sh
@@ -5,8 +5,8 @@
 # Variables.
 echo " "
 echo "INFO ! Checking for latest Minecraft Server version."
-MC_VERSION_OLD=1.19.3
-MC_VERSION=1.19.4
+MC_VERSION_OLD=1.19.2
+MC_VERSION=1.19.3
 
 # Main install (Debian).
 # Check for files in /MCserver and download if needed.
@@ -22,7 +22,7 @@ if [ -e /MCserver/MCserver_${MC_VERSION}.jar ]
 			mkdir /MCserver/old-server-versions/${MC_VERSION_OLD}
 			mv /MCserver/MCserver_${MC_VERSION_OLD}.jar /MCserver/old-server-versions/${MC_VERSION_OLD}
 			mv /MCserver/run_${MC_VERSION_OLD}.sh /MCserver/old-server-versions/${MC_VERSION_OLD}
-			wget --no-cache https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar -O /MCserver/MCserver_${MC_VERSION}.jar
+			wget --no-cache https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar -O /MCserver/MCserver_${MC_VERSION}.jar
 fi
 
 sleep 1
