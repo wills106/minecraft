c89a985fdad01aa3a2e44aa22384dd20c1b6d1e8
fithwum <34775371+fithwum@users.noreply.github.com>
2023-03-20 13:04:13 -0700

    structure changes

--- stable/1.19.4/files/Install_Script.sh
+++ stable/1.19.4/files/Install_Script.sh
@@ -8,7 +8,7 @@ echo "INFO ! Checking for latest Minecraft Server version."
 MC_VERSION_OLD=1.19.3
 MC_VERSION=1.19.4
 MC_SERVER_FILE=https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar
-MC_RUN_FILE=https://raw.githubusercontent.com/fithwum/minecraft/master/${MC_VERSION}/files/run.sh
+MC_RUN_FILE=https://raw.githubusercontent.com/fithwum/minecraft/master/stable/${MC_VERSION}/files/run.sh
 
 # Main install (Debian).
 # Check for files in /MCserver and download if needed.
